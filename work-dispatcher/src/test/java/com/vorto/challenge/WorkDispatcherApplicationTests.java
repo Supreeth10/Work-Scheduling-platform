627ae513c10f3a5868bb17de981b7f4132a8737d
supreeth10 <smuddu2@pdx.edu>
2025-09-25 23:10:14 -0700

    added initial springboot related folders

---
+++ work-dispatcher/src/test/java/com/vorto/challenge/WorkDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vorto.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

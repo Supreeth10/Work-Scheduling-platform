2b141beb268492bfd437d42547d519b72d5d7340
supreeth10 <smuddu2@pdx.edu>
2025-10-20 16:41:42 -0700

    removed commented code

--- work-dispatcher/src/main/java/com/vorto/challenge/common/DriverMapper.java
+++ work-dispatcher/src/main/java/com/vorto/challenge/common/DriverMapper.java
@@ -10,10 +10,6 @@ import static com.vorto.challenge.common.JtsGeo.toLatLng;
 public final class DriverMapper {
     private DriverMapper() {}
     public static DriverDto toDto(Driver d) {
-//        Point p = d.getCurrentLocation();
-//        Double lat = (p == null) ? null : p.getY(); // y = lat
-//        Double lon = (p == null) ? null : p.getX(); // x = lon
-//        return new DriverDto(d.getId(), d.getName(), d.isOnShift(), lat, lon);
         Point point = d.getCurrentLocation();
         LocationDto locationDto = (point == null) ? null : toLatLng(point);
         return new DriverDto(d.getId(), d.getName(), d.isOnShift(), locationDto);

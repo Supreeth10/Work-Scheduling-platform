ed549ca1abadf0c6550bdeb7370b63b6af7ab12e
supreeth10 <smuddu2@pdx.edu>
2025-10-19 19:18:47 -0700

    added documentation related code for shift related endpoints

--- work-dispatcher/src/main/java/com/vorto/challenge/DTO/DriverEndShiftDto.java
+++ work-dispatcher/src/main/java/com/vorto/challenge/DTO/DriverEndShiftDto.java
@@ -1,11 +1,14 @@
 package com.vorto.challenge.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.Instant;
 import java.util.UUID;
 
+@Schema(description = "Shift end result")
 public record DriverEndShiftDto(
-        UUID shiftId,
-        UUID driverId,
-        Instant endTime
+        @Schema(example = "c8aa58bf-cb5f-476f-8dae-32668eb5a134") UUID shiftId,
+        @Schema(example = "55e1c83d-93bb-4b31-b8c2-80fff3333bf3") UUID driverId,
+        @Schema(type = "string", format = "date-time", example = "2025-10-20T02:02:57.488604Z") Instant endTime
 ) {
 }
--- work-dispatcher/src/main/java/com/vorto/challenge/DTO/StartShiftRequest.java
+++ work-dispatcher/src/main/java/com/vorto/challenge/DTO/StartShiftRequest.java
@@ -1,17 +1,22 @@
 package com.vorto.challenge.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 
+
+@Schema(description = "Start shift request with current location")
 public record StartShiftRequest(
         @NotNull(message = "latitude is required")
         @DecimalMin(value = "-90.0",  message = "latitude must be >= -90")
         @DecimalMax(value = "90.0",   message = "latitude must be <= 90")
+        @Schema(example = "39.7392")
         Double latitude,
 
         @NotNull(message = "longitude is required")
         @DecimalMin(value = "-180.0", message = "longitude must be >= -180")
         @DecimalMax(value = "180.0",  message = "longitude must be <= 180")
+        @Schema(example = "-104.9903")
         Double longitude
 ) {}
